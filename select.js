91e557fd7b70768187609c77367e90479745ffe8
mojib2014 <mojib_monir@yahoo.com>
2021-05-21 10:54:54 -0700

    fix the  case-declaration issue with switch statement

--- select.js
+++ select.js
@@ -80,14 +80,16 @@ function setupCustomElement(select) {
       case "Space":
         select.optionsCustomElement.classList.toggle("show");
         break;
-      case "ArrowUp":
+      case "ArrowUp": {
         const prevOption = select.options[select.selectedOptionIndex - 1];
         if (prevOption) select.selectValue(prevOption.value);
         break;
-      case "ArrowDown":
+      }
+      case "ArrowDown": {
         const nextOption = select.options[select.selectedOptionIndex + 1];
         if (nextOption) select.selectValue(nextOption.value);
         break;
+      }
       case "Enter":
       case "Escape":
         select.optionsCustomElement.classList.remove("show");
